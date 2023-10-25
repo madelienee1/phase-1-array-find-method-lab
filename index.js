f8180cc6459db1ba006b8cbd26cdcbf2789b5052
Madeliene <madelienestiller@gmail.com>
2023-10-25 10:08:45 +1000

    method lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+
+function superbowlWin(element) {
+    const winningYear = element.find(object => {
+        return object.result === "W";
+    });
+
+    if (winningYear) {
+        return winningYear.year;
+    }
+    else {
+        return undefined;
+    }
+}
+
+console.log(superbowlWin(record));
+
+
+
+
